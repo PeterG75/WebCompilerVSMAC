eace0706c7269b6242648f13cef0b5c711b78f3a
Ramon F. Mendes <ramon@misoftware.rs>
2017-09-03 22:17:42 -0300

    GO

---
+++ WebCompiler/CtxCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+using MonoDevelop.Ide.Gui.Components;
+
+namespace WebCompiler
+{
+	public enum CtxCommands
+	{
+		COMPILE	
+	}
+
+	public class CtxCommandHandler : NodeCommandHandler
+	{
+		[CommandHandler(CtxCommands.COMPILE)]
+		protected void OnShowProperties ()
+		{
+			MessageService.ShowMessage("hello");
+		}
+
+		public override void ActivateItem()
+		{
+			base.ActivateItem();
+		}
+	}
+}
---
+++ WebCompiler/Properties/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+	"WebCompiler",
+	Namespace = "WebCompiler",
+	Version = "1.0"
+)]
+
+[assembly: AddinName("WebCompiler")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("WebCompiler")]
+[assembly: AddinAuthor("midiway")]
